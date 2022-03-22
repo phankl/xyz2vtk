ce7dd373c4bd1d16a0cca67fefdfbe8dd9cd4708
phankl <philipp.kloza@gmail.com>
2022-03-22 15:31:37 +0000

    2022 update

--- io.h
+++ io.h
@@ -2,9 +2,12 @@
 #define io_header
 
 #include <algorithm>
+#include <cctype>
+#include <cfloat>
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <locale>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -17,12 +20,17 @@
 using namespace std;
 
 namespace io {
-  vector<Timestep> readXYZ(string);
+  vector<Timestep> readXYZ(string, int);
+  vector<Timestep> readLMP(string);
   void writeVTK(int, double, const vector<Timestep>&, string);
 
   vector<XYZ> firstNode(int, double, const XYZ&, const XYZ&);
   vector<XYZ> centralNode(const XYZ&, const XYZ&, const XYZ&, const vector<XYZ>&);
   vector<XYZ> lastNode(const XYZ&, const XYZ&, const vector<XYZ>&);
+
+  static inline void ltrim(string&);
+  static inline void rtrim(string&);
+  static inline void trim(string&);
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -12,35 +12,53 @@ using namespace io;
 
 int main(int argc, char* argv[])
 {
-  string xyzFileName;
+  string inputFileName;
   string vtkFileName;
+  string typeNumberString;
   string radiusString;
   string sidesString;
   double radius;
+  int typeNumber;
   int sides = SIDES;
 
-  if (argc == 4) {
-    xyzFileName = argv[1];
-    vtkFileName = argv[2];
-    radiusString = argv[3];
-    radius = stod(radiusString);
-  }
-  else if (argc == 5) {
-    xyzFileName = argv[1];
-    vtkFileName = argv[2];
-    radiusString = argv[3];
-    sidesString = argv[4];
-    radius = stod(radiusString);
-    sides = stoi(sidesString);
-  }
-  else {
-    cout << "Incorrect input format. Two input formats possible:" << endl;
-    cout << "xyz2vtk XYZ_FILE_NAME VTK_FILE_NAME CNT_RADIUS" << endl;
-    cout << "xyz2vtk XYZ_FILE_NAME VTK_FILE_NAME CNT_RADIUS POLYGON_SIDES" << endl;
+  if (argc < 4 || argc > 6) {
+    cout << "Incorrect input format. The following input formats are possible:" << endl;
+    cout << "xyz2vtk LMP_FILE_NAME VTK_FILE_NAME CNT_RADIUS" << endl;
+    cout << "xyz2vtk LMP_FILE_NAME VTK_FILE_NAME CNT_RADIUS POLYGON_SIDES" << endl;
+    cout << "xyz2vtk XYZ_FILE_NAME VTK_FILE_NAME TYPE_NUMBER CNT_RADIUS" << endl;
+    cout << "xyz2vtk XYZ_FILE_NAME VTK_FILE_NAME TYPE_NUMBER CNT_RADIUS POLYGON_SIDES" << endl;
     return 1;
   }
 
-  vector<Timestep> timesteps = io::readXYZ(xyzFileName);
+  inputFileName = argv[1];
+  vtkFileName = argv[2];
+
+  size_t xyzFound = inputFileName.find(".xyz");
+  vector<Timestep> timesteps;
+
+  if (xyzFound != string::npos) {
+    typeNumberString = argv[3];
+    radiusString = argv[4];
+    typeNumber = stoi(typeNumberString);
+    radius = stod(radiusString);
+    if (argc == 6) {
+      sidesString = argv[5];
+      sides = stoi(sidesString);
+    }
+
+    timesteps = io::readXYZ(inputFileName, typeNumber);
+  }
+  else {
+    radiusString = argv[3];
+    radius = stod(radiusString);
+    if (argc == 5) {
+      sidesString = argv[4];
+      sides = stoi(sidesString);
+    }
+
+    timesteps = io::readLMP(inputFileName);
+  }
+
   io::writeVTK(sides, radius, timesteps, vtkFileName);
 
   return 0;
